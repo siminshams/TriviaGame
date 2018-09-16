ecf89250c19aa8f59a5ae1e454f1e8940660dea8
Simin Shamsfallah <Simin.Shamsfallah@Earlens.com>
2018-09-16 15:04:12 -0700

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -52,7 +52,7 @@ $(document).ready(function () {
         answer: 3,
     }];
 
-    var gifArray = ["q1","q2","q3","q4","q5","q6","q7","q8","q9","q10"];
+    var gifArray = ["Q1","Q2","Q3","Q4","Q5","Q6","Q7","Q8","Q9","Q10"];
 
 
     var timeLeft;
